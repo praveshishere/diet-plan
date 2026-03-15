8bab8b434fdbe64631b435745663e6d4a3f03f08
Prince Kunwar <prince.kunwar@xcelore.com>
2026-03-15 20:30:33 +0530

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
 import DietPlan from "./components/DietPlan";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className="text-red-500">
-        <DietPlan />
-      </div>
-    </>
-  )
+  return <DietPlan />;
 }
 
 export default App
